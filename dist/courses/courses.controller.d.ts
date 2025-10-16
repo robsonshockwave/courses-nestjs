ff8c00799997ac9adbc010fef7c652fe024e94b1
robson arruda <robsonshockwavegithub@gmail.com>
2025-10-16 19:00:01 -0300

    criando o service de courses com dados mockados

--- dist/courses/courses.controller.d.ts
+++
@@ -1,7 +0,0 @@
-export declare class CoursesController {
-    findAll(response: any): any;
-    findOne(id: string): string;
-    create(body: any): any;
-    update(id: string, body: any): string;
-    remove(id: string): string;
-}
