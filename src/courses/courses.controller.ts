97fe38e4a793cf1071578f6e686e47d124e58ced
robson arruda <robsonshockwavegithub@gmail.com>
2025-10-16 19:10:41 -0300

    Injetando o Service no Controller para usar os métodos

--- src/courses/courses.controller.ts
+++ src/courses/courses.controller.ts
@@ -6,9 +6,8 @@ import {
   HttpCode,
   HttpStatus,
   Param,
-  Patch,
   Post,
-  Res,
+  Put,
 } from '@nestjs/common';
 import { CoursesService } from './courses.service';
 
@@ -17,29 +16,28 @@ export class CoursesController {
   constructor(private readonly coursesService: CoursesService) {}
 
   @Get('list')
-  findAll(@Res() response) {
-    return response.status(200).json({ message: 'Listagem de cursos' });
+  findAll() {
+    return this.coursesService.findAll();
   }
 
   @Get(':id')
-  findOne(@Param('id') id: string) {
-    return `Curso com ID ${id}`;
+  findOne(@Param('id') id: number) {
+    return this.coursesService.findOne(+id);
   }
 
   @Post()
   create(@Body() body) {
-    return body;
+    return this.coursesService.create(body);
   }
 
-  @Patch(':id')
-  update(@Param('id') id: string, @Body() body) {
-    console.log(body);
-    return `Update course with ID ${id}`;
+  @Put(':id')
+  update(@Param('id') id: number, @Body() body) {
+    return this.coursesService.update(+id, body);
   }
 
   @HttpCode(HttpStatus.NO_CONTENT)
   @Delete(':id')
-  remove(@Param('id') id: string) {
-    return `Delete course with ID ${id}`;
+  remove(@Param('id') id: number) {
+    return this.coursesService.remove(+id);
   }
 }
