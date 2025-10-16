8e18946d2ca319a45afad103f4e699e47922dda4
robson arruda <robsonshockwavegithub@gmail.com>
2025-10-15 23:25:11 -0300

    criando o controller courses

---
+++ dist/courses/courses.controller.d.ts
@@ -0,0 +1,4 @@
+export declare class CoursesController {
+    findAll(): string;
+    findOne(id: string): string;
+}
---
+++ src/courses/courses.controller.ts
@@ -0,0 +1,14 @@
+import { Controller, Get, Param } from '@nestjs/common';
+
+@Controller('courses')
+export class CoursesController {
+  @Get('list')
+  findAll() {
+    return 'Listagem de cursos';
+  }
+
+  @Get(':id')
+  findOne(@Param('id') id: string) {
+    return `Curso com ID ${id}`;
+  }
+}
