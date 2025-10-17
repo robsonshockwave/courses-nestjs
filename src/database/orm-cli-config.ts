189bd7ffc0dba2322d181eeac014d5fd80d4c95a
robson arruda <robsonshockwavegithub@gmail.com>
2025-10-17 20:08:28 -0300

    criando a tebela de tags através de migration

--- src/database/orm-cli-config.ts
+++ src/database/orm-cli-config.ts
@@ -1,9 +1,10 @@
 import { DataSource } from 'typeorm';
 import { dataSourceOptions } from './database.module';
 import { CreateCoursesTable1760740796481 } from 'src/migrations/1760740796481-CreateCoursesTable';
+import { CreateTagsTable1760742118124 } from 'src/migrations/1760742118124-CreateTagsTable';
 
 export const dataSource = new DataSource({
   ...dataSourceOptions,
   synchronize: false,
-  migrations: [CreateCoursesTable1760740796481],
+  migrations: [CreateCoursesTable1760740796481, CreateTagsTable1760742118124],
 });
