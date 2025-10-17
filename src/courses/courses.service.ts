00f299baf51a2455a03cc140da13ba33ffc1149b
robson arruda <robsonshockwavegithub@gmail.com>
2025-10-17 13:47:27 -0300

    refatorando os métodos Create e Update com os relacionamentos e a inserção das tags na tabela de tags

--- src/courses/courses.service.ts
+++ src/courses/courses.service.ts
@@ -2,12 +2,18 @@ import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
 import { Course } from './entities/courses.entity';
 import { Repository } from 'typeorm';
 import { InjectRepository } from '@nestjs/typeorm';
+import { Tag } from './entities/tags.entity';
+import { CreateCourseDTO } from './dto/create-course.dto';
+import { UpdateCourseDTO } from './dto/update-course.dto';
 
 @Injectable()
 export class CoursesService {
   constructor(
     @InjectRepository(Course)
     private readonly courseRepository: Repository<Course>,
+
+    @InjectRepository(Tag)
+    private readonly tagRepository: Repository<Tag>,
   ) {}
 
   async findAll() {
@@ -27,16 +33,30 @@ export class CoursesService {
     return course;
   }
 
-  async create(createCourseDTO: any) {
-    const course = this.courseRepository.create(createCourseDTO);
+  async create(createCourseDTO: CreateCourseDTO) {
+    const tags = await Promise.all(
+      createCourseDTO.tags.map((name) => this.preloadTagByName(name)),
+    );
+
+    const course = this.courseRepository.create({
+      ...createCourseDTO,
+      tags,
+    });
 
     return this.courseRepository.save(course);
   }
 
-  async update(id: number, updateCourseDTO: any) {
+  async update(id: number, updateCourseDTO: UpdateCourseDTO) {
+    const tags =
+      updateCourseDTO.tags &&
+      (await Promise.all(
+        updateCourseDTO.tags.map((name) => this.preloadTagByName(name)),
+      ));
+
     const course = await this.courseRepository.preload({
       ...updateCourseDTO,
       id,
+      tags,
     });
 
     if (!course) {
@@ -60,5 +80,15 @@ export class CoursesService {
     }
 
     return this.courseRepository.remove(course);
+  }
+
+  private async preloadTagByName(name: string): Promise<Tag> {
+    const tag = await this.tagRepository.findOne({ where: { name } });
+
+    if (tag) {
+      return tag;
+    }
+
+    return this.tagRepository.create({ name });
   }
 }
