40b96e4c313704b1410f13a51eaaec1e5c62acd0
robson arruda <robsonshockwavegithub@gmail.com>
2025-10-17 13:10:34 -0300

    criando a entity tags e fazendo o relacionamento many-to-many com courses

--- src/database/database.module.ts
+++ src/database/database.module.ts
@@ -2,6 +2,7 @@ import { Module } from '@nestjs/common';
 import { Course } from 'src/courses/entities/courses.entity';
 import { DataSourceOptions } from 'typeorm';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { Tag } from 'src/courses/entities/tags.entity';
 
 export const dataSourceOptions: DataSourceOptions = {
   type: 'postgres',
@@ -10,7 +11,7 @@ export const dataSourceOptions: DataSourceOptions = {
   username: 'postgres',
   password: 'docker',
   database: 'devtraining',
-  entities: [Course],
+  entities: [Course, Tag],
   synchronize: true,
 };
 
