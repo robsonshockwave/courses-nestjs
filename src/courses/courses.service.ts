e2f27508e4ab9ee6b02f51e92146257e88eb380b
robson arruda <robsonshockwavegithub@gmail.com>
2025-10-17 13:50:55 -0300

    Refatorando os métodos findAll e findOne de courses para buscar os dados relacionados (tags)

--- src/courses/courses.service.ts
+++ src/courses/courses.service.ts
@@ -17,11 +17,16 @@ export class CoursesService {
   ) {}
 
   async findAll() {
-    return this.courseRepository.find();
+    return this.courseRepository.find({
+      relations: ['tags'],
+    });
   }
 
   async findOne(id: number) {
-    const course = await this.courseRepository.findOne({ where: { id } });
+    const course = await this.courseRepository.findOne({
+      where: { id },
+      relations: ['tags'],
+    });
 
     if (!course) {
       throw new HttpException(
