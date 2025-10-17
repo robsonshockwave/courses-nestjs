43753999d28e63e96392d738f09b89e2b9e137a3
robson arruda <robsonshockwavegithub@gmail.com>
2025-10-16 22:58:57 -0300

    configurando o typeorm e criando a entidade de course

---
+++ src/database/database.module.ts
@@ -0,0 +1,26 @@
+import { Module } from '@nestjs/common';
+import { Course } from 'src/courses/entities/courses.entity';
+import { DataSourceOptions } from 'typeorm';
+import { TypeOrmModule } from '@nestjs/typeorm';
+
+export const dataSourceOptions: DataSourceOptions = {
+  type: 'postgres',
+  host: 'localhost',
+  port: 5432,
+  username: 'postgres',
+  password: 'docker',
+  database: 'devtraining',
+  entities: [Course],
+  synchronize: true,
+};
+
+@Module({
+  imports: [
+    TypeOrmModule.forRootAsync({
+      useFactory: async () => {
+        return dataSourceOptions;
+      },
+    }),
+  ],
+})
+export class DatabaseModule {}
