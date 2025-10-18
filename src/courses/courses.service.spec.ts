b80eb38655ba0a08056f089f0948b3dc70d1aeb2
robson arruda <robsonshockwavegithub@gmail.com>
2025-10-18 16:46:45 -0300

    criando o teste unitario de create course

--- src/courses/courses.service.spec.ts
+++ src/courses/courses.service.spec.ts
@@ -1,14 +1,76 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { CoursesService } from './courses.service';
+import { Repository } from 'typeorm';
+import { Course } from './entities/courses.entity';
+import { Tag } from './entities/tags.entity';
+import { CreateCourseDTO } from './dto/create-course.dto';
 
 describe('CoursesService', () => {
   let service: CoursesService;
+  let courseRepository: jest.Mocked<Repository<Course>>;
+  let tagRepository: jest.Mocked<Repository<Tag>>;
+
+  let id: string;
+  let date: Date;
 
   beforeEach(async () => {
-    service = new CoursesService();
+    id = '1e38a115-2bdd-4c12-8d0b-fc3f54741ba4';
+    date = new Date();
+
+    const module: TestingModule = await Test.createTestingModule({
+      providers: [
+        CoursesService,
+        {
+          provide: 'COURSES_REPOSITORY',
+          useValue: {
+            create: jest.fn(),
+            save: jest.fn(),
+          },
+        },
+        {
+          provide: 'TAGS_REPOSITORY',
+          useValue: {
+            findOne: jest.fn(),
+            create: jest.fn(),
+          },
+        },
+      ],
+    }).compile();
+
+    service = module.get<CoursesService>(CoursesService);
+    courseRepository = module.get('COURSES_REPOSITORY');
+    tagRepository = module.get('TAGS_REPOSITORY');
   });
 
   it('should be defined', () => {
     expect(service).toBeDefined();
+  });
+
+  it('should create a course', async () => {
+    const expectOutputTags = [{ id, name: 'nestjs', created_at: date }];
+    const expectOutputCourse = {
+      id,
+      name: 'Teste',
+      description: 'Test description',
+      tags: expectOutputTags,
+      created_at: date,
+    };
+
+    tagRepository.findOne.mockResolvedValue(null);
+    tagRepository.create.mockReturnValue(expectOutputTags[0] as Tag);
+    courseRepository.create.mockReturnValue(expectOutputCourse as Course);
+    courseRepository.save.mockResolvedValue(expectOutputCourse as Course);
+
+    const createCourseDTO: CreateCourseDTO = {
+      name: 'Teste',
+      description: 'Test description',
+      tags: ['nestjs'],
+    };
+
+    const result = await service.create(createCourseDTO);
+
+    expect(courseRepository.create).toHaveBeenCalled();
+    expect(courseRepository.save).toHaveBeenCalledWith(expectOutputCourse);
+    expect(result).toEqual(expectOutputCourse);
   });
 });
