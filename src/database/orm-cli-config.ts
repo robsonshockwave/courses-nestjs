e63cd7df2e3eabc082724937dcc9e868cdc94499
robson arruda <robsonshockwavegithub@gmail.com>
2025-10-17 21:41:13 -0300

    Criando a coluna coursesId na tabela courses_tags através de migração

--- src/database/orm-cli-config.ts
+++ src/database/orm-cli-config.ts
@@ -3,6 +3,7 @@ import { dataSourceOptions } from './database.module';
 import { CreateCoursesTable1760740796481 } from 'src/migrations/1760740796481-CreateCoursesTable';
 import { CreateTagsTable1760742118124 } from 'src/migrations/1760742118124-CreateTagsTable';
 import { CreateCoursesTagsTable1760746292249 } from 'src/migrations/1760746292249-CreateCoursesTagsTable';
+import { AddCoursesIdToCoursesTagsTable1760747551912 } from 'src/migrations/1760747551912-AddCoursesIdToCoursesTagsTable';
 
 export const dataSource = new DataSource({
   ...dataSourceOptions,
@@ -11,5 +12,6 @@ export const dataSource = new DataSource({
     CreateCoursesTable1760740796481,
     CreateTagsTable1760742118124,
     CreateCoursesTagsTable1760746292249,
+    AddCoursesIdToCoursesTagsTable1760747551912,
   ],
 });
