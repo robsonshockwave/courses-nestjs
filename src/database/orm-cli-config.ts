d305bd0618446eef01339f3b4866941a11cecef2
robson arruda <robsonshockwavegithub@gmail.com>
2025-10-17 21:15:28 -0300

    Criando a tabela courses_tags através de migração

--- src/database/orm-cli-config.ts
+++ src/database/orm-cli-config.ts
@@ -2,9 +2,14 @@ import { DataSource } from 'typeorm';
 import { dataSourceOptions } from './database.module';
 import { CreateCoursesTable1760740796481 } from 'src/migrations/1760740796481-CreateCoursesTable';
 import { CreateTagsTable1760742118124 } from 'src/migrations/1760742118124-CreateTagsTable';
+import { CreateCoursesTagsTable1760746292249 } from 'src/migrations/1760746292249-CreateCoursesTagsTable';
 
 export const dataSource = new DataSource({
   ...dataSourceOptions,
   synchronize: false,
-  migrations: [CreateCoursesTable1760740796481, CreateTagsTable1760742118124],
+  migrations: [
+    CreateCoursesTable1760740796481,
+    CreateTagsTable1760742118124,
+    CreateCoursesTagsTable1760746292249,
+  ],
 });
