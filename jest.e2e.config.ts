c4ee0a29b8f90518a556bb99bc7b07314a99f914
robson arruda <robsonshockwavegithub@gmail.com>
2025-10-19 12:24:24 -0300

    configurando teste e2e no projeto

---
+++ jest.e2e.config.ts
@@ -0,0 +1,10 @@
+export default {
+  moduleFileExtensions: ['ts', 'js', 'json'],
+  testRegex: '.*\\.e2e-spec\\.ts$',
+  transform: {
+    '^.+\\.(t|j)s$': 'ts-jest',
+  },
+  collectCoverageFrom: ['**/*.(t|j)s'],
+  coverageDirectory: '../coverage',
+  testEnvironment: 'node',
+};
