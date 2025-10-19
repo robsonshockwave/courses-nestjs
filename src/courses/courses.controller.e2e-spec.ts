6d1dab0446841839c9df6cbf9a3087147b441e75
robson arruda <robsonshockwavegithub@gmail.com>
2025-10-19 14:28:09 -0300

    criando teste e2e de lista todos os cursos, curso especifico, editar e remover

--- src/courses/courses.controller.e2e-spec.ts
+++ src/courses/courses.controller.e2e-spec.ts
@@ -55,4 +55,91 @@ describe('CoursesController e2e tests', () => {
       expect(response.body.tags[1].name).toEqual(data.tags[1]);
     });
   });
+
+  describe('/courses (GET)', () => {
+    it('should list all courses', async () => {
+      await request(app.getHttpServer())
+        .post('/courses')
+        .send(data)
+        .expect(201);
+
+      const response = await request(app.getHttpServer())
+        .get('/courses/list')
+        .expect(200);
+
+      expect(response.body[0].id).toBeDefined();
+      expect(response.body[0].name).toEqual(data.name);
+      expect(response.body[0].description).toEqual(data.description);
+      expect(response.body[0].created_at).toBeDefined();
+      response.body.map((course) =>
+        expect(course).toEqual({
+          id: course.id,
+          name: data.name,
+          description: data.description,
+          created_at: course.created_at,
+          tags: [...course.tags],
+        }),
+      );
+    });
+
+    it('should find a course by id', async () => {
+      const course = await request(app.getHttpServer())
+        .post('/courses')
+        .send(data)
+        .expect(201);
+
+      const response = await request(app.getHttpServer())
+        .get(`/courses/${course.body.id}`)
+        .expect(200);
+
+      expect(response.body.id).toBeDefined();
+      expect(response.body.name).toEqual(data.name);
+      expect(response.body.description).toEqual(data.description);
+      expect(response.body.created_at).toBeDefined();
+      expect(response.body.tags[0].name).toEqual(data.tags[0]);
+      expect(response.body.tags[1].name).toEqual(data.tags[1]);
+    });
+  });
+
+  describe('/courses (PUT)', () => {
+    it('should update a course', async () => {
+      const response = await request(app.getHttpServer())
+        .post('/courses')
+        .send(data)
+        .expect(201);
+
+      const updatedData = {
+        name: 'Node.js Updated',
+        description: 'Node.js Updated',
+        tags: ['nodejs', 'nestjs', 'backend'],
+      };
+
+      const updateResponse = await request(app.getHttpServer())
+        .put(`/courses/${response.body.id}`)
+        .send(updatedData)
+        .expect(200);
+
+      expect(updateResponse.body.id).toEqual(response.body.id);
+      expect(updateResponse.body.name).toEqual(updatedData.name);
+      expect(updateResponse.body.description).toEqual(updatedData.description);
+      expect(updateResponse.body.tags).toHaveLength(3);
+      expect(updateResponse.body.tags[0].name).toEqual(updatedData.tags[0]);
+      expect(updateResponse.body.tags[1].name).toEqual(updatedData.tags[1]);
+      expect(updateResponse.body.tags[2].name).toEqual(updatedData.tags[2]);
+    });
+  });
+
+  describe('/courses (DELETE)', () => {
+    it('should delete a course', async () => {
+      const response = await request(app.getHttpServer())
+        .post('/courses')
+        .send(data)
+        .expect(201);
+
+      await request(app.getHttpServer())
+        .delete(`/courses/${response.body.id}`)
+        .expect(204)
+        .expect({});
+    });
+  });
 });
